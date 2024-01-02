95ff88b8af94a9567d173aa072216c87ee087bed
rachelisraeli <r0504172638@gmail.com>
2024-01-02 02:05:30 +0200

    my_hw

--- classroom_managment.py
+++ classroom_managment.py
@@ -33,8 +33,20 @@ classroom = [
     },
 ]
 
+#פונקצית עזר שתקבל שם של תלמיד ותחזיר את האינדקס שלו ברשימה
+# V
+def index_of_student(name):
+    for i, student in enumerate(classroom):
+        if student['name'] == name:
+           return i
+    #return classroom.index(name)[name]
 
+# V
 def add_student(name, email=None):
+    if(email==None):
+        classroom.append({'name': name,'email':f'{name.lower()}@example.com','grades':[]})
+    else:
+        classroom.append({'name': name,'email':email,'grades':[]})
     """Add a new student to the classroom
     with the following keys:
     'name': the given name
@@ -44,29 +56,56 @@ def add_student(name, email=None):
     """
     pass
 
-
+# V
 def delete_student(name):
+    index=index_of_student(name)
+    classroom.remove(classroom[index])
     """Delete a student from the classroom"""
     pass
 
-
+# V
 def set_email(name, email):
+    classroom[index_of_student(name)]['email']=email
+    #classroom[index_of_student(name)].get('email')
     """Sets the email of the student"""
     pass
 
-
+# V
 def add_grade(name, profession, grade):
+    classroom[index_of_student(name)]['grades'].append((profession,grade))
     """Adds a new grade to the student grades"""
     pass
 
-
+# V
 def avg_grade(name, profession):
+    sum_of_grades=0
+    num_of_grades=0
+    for g in classroom[index_of_student(name)]['grades']:
+        if(g[0]==profession):
+            sum_of_grades+=g[1]
+            num_of_grades+=1
+    return sum_of_grades/num_of_grades
     """Returns the average of grades of the student
     in the specified profession
     """
     pass
 
-
+# V
 def get_professions(name):
+    l=[]
+    for g in classroom[index_of_student(name)]['grades']:
+        if(g[0] not in l):
+            l.append(g[0])
+    return l
     """Returns a list of unique professions that student has grades in"""
     pass
+
+#print(index_of_student('Charlie'))
+delete_student('Bob')
+set_email('Charlie','charlie2@example.com')
+add_grade('Bob','physics',100)
+print(avg_grade('Charlie','english'))
+print(get_professions('Charlie'))
+add_student('Abc','abc@gmail.com')
+add_student('Def')
+print(classroom)
